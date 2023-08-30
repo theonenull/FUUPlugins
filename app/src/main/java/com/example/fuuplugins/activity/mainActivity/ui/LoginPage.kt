b604aa927eee56f7b8610b5b75e891a97427925c
“沈轻腾“ <sqt18750016193@163.com>
2023-08-30 22:52:20 +0800

    feat(MainActivity->LoginPage): 添加密码显示与隐藏

--- app/src/main/java/com/example/fuuplugins/activity/mainActivity/ui/LoginPage.kt
+++ app/src/main/java/com/example/fuuplugins/activity/mainActivity/ui/LoginPage.kt
@@ -1,7 +1,11 @@
 package com.example.fuuplugins.activity.mainActivity.ui
 
 import android.graphics.BitmapFactory
+import android.graphics.drawable.ClipDrawable
+import androidx.compose.animation.Animatable
 import androidx.compose.animation.Crossfade
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -14,9 +18,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -28,14 +36,21 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.drawscope.clipRect
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -115,7 +130,19 @@ fun LoginDetailsPage(
     retryGetVerificationCode: () -> Unit = {},
     login : () -> Unit = {}
 ){
-
+    var isPassword by remember {
+        mutableStateOf(true)
+    }
+    val animationProgram = remember {
+        androidx.compose.animation.core.Animatable(0f)
+    }
+    LaunchedEffect(isPassword){
+        if(isPassword){
+            animationProgram.animateTo(0f, tween(300))
+        }else{
+            animationProgram.animateTo(1f, tween(300))
+        }
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -147,8 +174,33 @@ fun LoginDetailsPage(
             },
             maxLines = 1,
             singleLine = true,
-            visualTransformation =  PasswordVisualTransformation(),
+            visualTransformation =  if (isPassword) PasswordVisualTransformation() else VisualTransformation.None,
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.Lock,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .drawBehind {
+                            clipRect(right = size.width*animationProgram.value) {
+                                drawLine(
+                                    Color.Black,
+                                    Offset(0f, 0f),
+                                    Offset(size.width, size.height),
+                                    strokeWidth = 5f,
+                                    cap = StrokeCap.Round
+                                )
+                            }
+
+                        }
+                        .clickable {
+                            isPassword = !isPassword
+                        }
+                        .padding(5.dp)
+
+                )
+            }
         )
         CaptchaLine(
             verificationCodeState = verificationCodeState,
